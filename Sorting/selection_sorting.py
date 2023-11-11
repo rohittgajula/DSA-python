f64af6d8f5c075f136b25aa115735e06e8b772ff
Rohit Gajula <rohitgajula27@gmail.com>
2023-11-11 21:59:17 +0530

    nc

---
+++ Sorting/selection_sorting.py
@@ -0,0 +1,19 @@
+# sort the given array in to non-decending order using selection sorting method
+
+# arr = [5,4,3,2,1]
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        minIndexValue = i
+        for j in range(i+1, n):
+            if arr[j] < arr[minIndexValue]:
+                minIndexValue = j
+                temp = arr[i]
+                arr[i] = arr[minIndexValue]
+                arr[minIndexValue] = temp
+    return arr
+
+
+arr = [5,4,3,2,1]
+print(selection_sort(arr))
